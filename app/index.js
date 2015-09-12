e7954488aaf9778ee03d1781051e6b6b51bc50b4
Greg Dick <gregory.j.dick@vanderbilt.edu>
2015-09-12 16:23:21 -0500

    initial commit with boilerplate

---
+++ app/index.js
@@ -0,0 +1,26 @@
+var express = require('express');
+
+var routes = require('./routes');
+
+var app = module.exports = express();
+
+app.set('port', process.env.PORT || 3000);
+
+app.set('views', './views');
+
+app.set('view engine', 'jade');
+
+app.use(express.static('www'));
+
+app.use('/', routes);
+
+startNodeListener();
+
+function startNodeListener() {
+  var server = app.listen(app.get('port'), function () {
+    var port = server.address().port;
+
+    console.log(`Server listening on port ${port}...`);
+  });
+};
+
