eb42921b5e084087e21e3f6228542532f76c6fc2
Greg Dick <gregory.j.dick@vanderbilt.edu>
2015-09-14 11:40:53 -0500

    game function complete but does not loop into next game

--- www/js/main.js
+++ www/js/main.js
@@ -2,29 +2,44 @@ var turn = 'X';
 var winner;
 var $squares = $('.square');
 
-$squares.click(turn, function () {
-  if(!$(this).text()){
-    $(this).text(turn);
-  }
-});
 
-
-function game (winner, turn) {
+function game () {
+  var board = readBoard();
+  winner = checkForWin(board);
+  console.log(winner);
   if(winner){
+    gameOver(winner);
     return winner;
   }else{
-    var board = readBoard();
-    winner = checkForWin(board);
     turn = turn === 'X' ? 'O' : 'X';
-    game(winner, turn);
+    $squares.click(turn, function () {
+      if(!$(this).text()){
+        $(this).text(turn);
+        game();
+      }
+    });
   }
 };
 
 function checkForWin (board) {
+  //there are 8 winning patterns
   //if three in a row
   //return 'X' or 'O'
-  //else return null;
+  //else return null
+  var isItOver = checkForThree(board, 0, 1, 2) || checkForThree(board, 3, 4, 5) || checkForThree(board, 6, 7, 8) || checkForThree(board, 0, 3, 6) || checkForThree(board, 1, 4, 7) || checkForThree(board, 2, 5, 8)|| checkForThree(board, 0, 4, 8) || checkForThree(board, 2, 4, 6);
+  return isItOver;
 };
+
+function checkForThree (board, x, y, z) {
+  if(board[x] && board[x] === board[y] && board[y] === board[z]){
+    return {
+      who : board[x],
+      how : [x, y, z]
+    };
+  }else{
+    return null;
+  }
+}
 
 function readBoard () {
   //iterate over board elements
@@ -43,4 +58,17 @@ function readBoard () {
   });
   return map;
 }
+
+function gameOver (winner) {
+  $squares.off();
+  $($squares[winner.how[0]]).addClass('win');
+  $($squares[winner.how[1]]).addClass('win');
+  $($squares[winner.how[2]]).addClass('win');
+  winner.who = winner.who === 1 ? 'X' : 'O';
+  $('.winner').append('<h1>' + winner.who + ' wins! Play again?</h1>')
+}
+
+
+//play ball
+game(turn);
 
